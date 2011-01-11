6f3552133165fea931fbf3024fbe1cdd4acccf89
Johnny Morrice <spoon@killersmurf.com>
2011-01-11 16:01:30 +0000

    Top level runner

---
+++ app/selfmodifier.rb
@@ -0,0 +1,16 @@
+# Our SelfModifier module
+module SelfModifier
+
+	# The top level directory
+	DIR = File.dirname __FILE__
+
+	$:.push DIR
+
+	# Load all controllers
+	
+	# Load all models
+	
+	# Load all user classes
+	
+
+end
